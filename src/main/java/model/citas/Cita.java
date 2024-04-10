20ba6b5fb07b128eeb5fa7b6331bfbf65dd29707
esedesofiaaa <sofa.santis2@aiesec.net>
2024-04-10 11:01:52 -0500

    Cambios estructura de Citas
    Se unifica cita Control y cita Valoracion en una sola clase

--- src/main/java/model/citas/Cita.java
+++ src/main/java/model/citas/Cita.java
@@ -1,20 +1,80 @@
 package model.citas;
+
+import estructurasDatos.GeneradorCodigo;
+
+/**
+ * Clase que representa una cita
+ */
 public class Cita {
-
+    private String idCita;
     private String idPaciente;
+    private boolean pagado = false;
+    private boolean tomado = false;
+    private String costo;
+    private String motivoCita;
+    private String especialidad;
 
-    private boolean pagado;
+    /**
+     * Instancia de la clase GeneradorCodigo
+     */
+    GeneradorCodigo generadorCodigo = new GeneradorCodigo();
 
-    private boolean tomado;
+    /**
+     * Constructor de la clase Cita
+     * @param costo Costo de la cita
+     * @param especialidad Especialidad de la cita
+     * @param idCita Identificador de la cita
+     * @param idPaciente Identificador del paciente
+     * @param motivoCita Motivo de la cita
+     * @param pagado Estado de pago de la cita
+     * @param tomado Estado de toma de la cita
+     */
+    public Cita(String costo, String especialidad, String idCita, String idPaciente, String motivoCita, boolean pagado, boolean tomado) {
+        this.costo = costo;
 
+        if (motivoCita == "CONTROL") {
+            this.costo = "Gratis";
+        } else if (motivoCita == "VALORACION" && especialidad != "MEDICINA_GENERAL") {
+            this.costo = "4500";
+        } else if (motivoCita == "VALORACION" && especialidad == "MEDICINA_GENERAL") {
+            this.costo = "2500";
+        }
 
-    public Cita() {
+        this.idCita = generadorCodigo.generarCodigo(motivoCita, especialidad);
+        this.idPaciente = idPaciente;
+        if (motivoCita == "CONTROL" || motivoCita == "VALORACION") {
+            this.motivoCita = motivoCita;
+        }
+        else {
+            throw new IllegalArgumentException("Motivo de cita no válido: " + motivoCita);
+        }
+
+        this.pagado = pagado;
+        this.tomado = tomado;
     }
 
-    public Cita(String idPaciente, boolean pagado) {
-        this.idPaciente = idPaciente;
-        this.tomado = false;
-        this.pagado = pagado;
+    public String getCosto() {
+        return costo;
+    }
+
+    public void setCosto(String costo) {
+        this.costo = costo;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getIdCita() {
+        return idCita;
+    }
+
+    public void setIdCita(String idCita) {
+        this.idCita = idCita;
     }
 
     public String getIdPaciente() {
@@ -23,6 +83,14 @@ public class Cita {
 
     public void setIdPaciente(String idPaciente) {
         this.idPaciente = idPaciente;
+    }
+
+    public String getMotivoCita() {
+        return motivoCita;
+    }
+
+    public void setMotivoCita(String motivoCita) {
+        this.motivoCita = motivoCita;
     }
 
     public boolean isPagado() {
@@ -41,12 +109,20 @@ public class Cita {
         this.tomado = tomado;
     }
 
-
+    /**
+     * Método que permite obtener la información de la cita
+     * @return Información de la cita
+     */
     @Override
     public String toString() {
         return "Cita{" +
-                "idPaciente='" + idPaciente + '\'' +
+                "idCita='" + idCita + '\'' +
+                ", idPaciente='" + idPaciente + '\'' +
                 ", pagado=" + pagado +
-                ", tomado=" + tomado;
+                ", tomado=" + tomado +
+                ", costo='" + costo + '\'' +
+                ", motivoCita='" + motivoCita + '\'' +
+                ", especialidad='" + especialidad + '\'' +
+                '}';
     }
 }
